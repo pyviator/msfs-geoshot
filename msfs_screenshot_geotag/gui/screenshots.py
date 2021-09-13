fa5653171a52588e7edc1aa6bfbba3a0d23ef536
pyviator <90870672+pyviator@users.noreply.github.com>
2021-09-14 00:54:26 +0200

    Implement screenshot service

---
+++ msfs_screenshot_geotag/gui/screenshots.py
@@ -0,0 +1,67 @@
+import time
+from datetime import datetime
+from pathlib import Path
+from typing import Optional
+
+import tzlocal
+from PyQt5.QtGui import QGuiApplication
+
+from ..exif_service import ExifLocationData, ExifService
+from ..sim_service import SimService, SimServiceError
+
+
+class ScreenShotService:
+
+    _date_format = "%Y-%m-%d-%H%M%S"
+    _file_stem_format = "MSFS_Screenshot_{date}"
+    _extension = "png"
+
+    def __init__(
+        self, sim_service: SimService, exif_service: ExifService, target_folder: Path
+    ):
+        self._sim_service = sim_service
+        self._exif_service = exif_service
+        self._target_folder = target_folder
+
+    def take_screenshot(self) -> Optional[Path]:
+        try:
+            location_data = self._sim_service.get_current_location()
+        except SimServiceError as e:
+            print(e)
+            return None
+
+        out_path = self._get_new_screenshot_path(location_data)
+
+        self._grab_screenshot(out_path)
+        if location_data:
+            self._write_metadata(location_data, out_path)
+
+        return out_path
+
+    def _get_new_screenshot_path(
+        self, exif_location_data: Optional[ExifLocationData] = None
+    ) -> Path:
+        if exif_location_data:
+            capture_time: float = exif_location_data.gps_datestamp
+        else:
+            capture_time = time.time()
+
+        local_timezone = tzlocal.get_localzone()
+        capture_datetime = datetime.fromtimestamp(capture_time, tz=local_timezone)
+        datetime_string = capture_datetime.strftime(self._date_format)
+
+        file_stem = self._file_stem_format.format(date=datetime_string)
+        file_name = f"{file_stem}.{self._extension}"
+
+        return self._target_folder / file_name
+
+    def _grab_screenshot(self, out_path: Path):
+        # TODO: identify actual window rather than using primary screen
+        active_screen = QGuiApplication.primaryScreen()
+        root_window = active_screen.grabWindow(0)  # type: ignore
+        root_window.save(str(out_path), quality=100)
+
+    def _write_metadata(self, location_data: ExifLocationData, screenshot: Path):
+        self._exif_service.write_data(
+            exif_location_data=location_data, image=screenshot
+        )
