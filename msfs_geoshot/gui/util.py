ac1ab7828db331f98e4c000a9e3223cfd07d0f09
pyviator <90870672+pyviator@users.noreply.github.com>
2021-09-26 15:02:02 +0200

    Add non-blocking wait function

--- msfs_geoshot/gui/util.py
+++ msfs_geoshot/gui/util.py
@@ -1,6 +1,16 @@
-from PyQt5.QtCore import QUrl
-from PyQt5.QtGui import QDesktopServices
+import time
+
+from PyQt5.QtCore import QCoreApplication, QUrl
+from PyQt5.QtGui import QDesktopServices, QGuiApplication
 
 
 def open_url(url: str):
     QDesktopServices.openUrl(QUrl(url))
+
+
+def wait(duration: float):
+    """Sleep for a set number of seconds without blocking main UI thread"""
+    steps = int(duration * 10)
+    for _ in range(steps):
+        QGuiApplication.processEvents()
+        time.sleep(0.1)
